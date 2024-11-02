0dacf15c75cd2bbb5fdcac24481f18c1a47f34d1
johnUfo <muydinovumidjon17@gmail.com>
2024-11-02 22:35:17 +0500

    category create and category edit go back button is working

---
+++ src/main/java/fast_food_website/controller/FoodController.java
@@ -0,0 +1,10 @@
+package fast_food_website.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FoodController {
+
+//    private FoodService foodService
+
+}
